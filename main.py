0c88679e9fd8e399e9a0f5185da59191ed46ed92
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2023-09-21 11:45:52 -0500

    refactor: restructure and add typing

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
-import argparse
-import os
-import json
+"""
+OPRF Asset Updater
+Copyright (C) 2023  BobDotCom
 
-import requests
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+import argparse
+from oprf_asset_updater import main
 
 parser = argparse.ArgumentParser(
     prog="OPRF Asset Updater",
@@ -72,78 +86,4 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-version_json = os.path.join(args.directory, args.version_json)
-
-with open(version_json, "r") as f:
-    local_version_data = json.load(f)
-
-
-def build_remote_url(path: str) -> str:
-    return f"https://raw.githubusercontent.com/{args.repository}/{args.branch}/{path}"
-
-
-def save_local_version_data():
-    with open(version_json, "w") as f:
-        json.dump(local_version_data, f, indent=2)
-
-
-def compare_versions(first, second):
-    if first is None or second is None:
-        return None
-    val1 = [int(val) for val in first.split(".")]
-    val2 = [int(val) for val in second.split(".")]
-    if val1[0] > val2[0]:
-        return 3
-    if val1[:2] > val2[:2]:
-        return 2
-    if val1 > val2:
-        return 1
-    if val1 == val2:
-        return 0
-    if val2[0] > val1[0]:
-        return -3
-    if val2[:2] > val1[:2]:
-        return -2
-    if val2 > val1:
-        return -1
-
-
-with requests.get(build_remote_url("versions.json")) as response:
-    remote_version_data = response.json()
-
-if args.include == "*":
-    keys_to_check = list(local_version_data.keys())
-elif args.include == "":
-    keys_to_check = []
-else:
-    keys_to_check = args.include.split(",")
-
-if args.exclude != "":
-    for key in args.exclude.split(","):
-        keys_to_check.remove(key)
-
-for key, value in {key: local_version_data[key] for key in keys_to_check}.items():
-    def update():
-        with requests.get(build_remote_url(remote_version_data[key]["path"])) as response:
-            response.raise_for_status()
-            with open(os.path.join(args.directory, value["path"]), "w") as f:
-                f.write(response.text)
-        print(f"Fetched {key} {value['version']}->{remote_version_data[key]['version']}")
-        value["version"] = remote_version_data[key]["version"]
-    match compare_versions(remote_version_data[key]["version"], value["version"]):
-        case 3:  # Remote is a major version bump ahead of us
-            if args.major == "true":
-                update()
-            else:
-                print(f"Remote {key} ({remote_version_data[key]['version']}) is a major version newer than ours ({value['version']}), skipping.")
-        case 2 | 1 | None:  # Remote is a minor/patch version bump ahead of us, or we don't have a saved version yet
-            update()
-        case -1 | -2 | -3:
-            newer_msg = f"Local {key} ({value['version']}) is newer than remote ({remote_version_data[key]['version']})"
-            if args.strict == "true":
-                raise RuntimeError(newer_msg)
-            else:
-                print(f"{newer_msg}, skipping.")
-        case 0:
-            print(f"{key} is up-to-date ({value['version']})")
-    save_local_version_data()
+main(args)
